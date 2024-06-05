55d4ae2ea7a31636a7cd65a6998ea14217614df5
Ivan <132115436+zIVANtheGREAT@users.noreply.github.com>
2024-06-05 12:44:21 -0700

    Update InputRead.v
    assigned res4_out and res5_out its concatenation and added some comments to describe what is going on. res4 and res5 become a form of barrel shifter.

--- sources_1/new/InputRead.v
+++ sources_1/new/InputRead.v
@@ -1,19 +1,11 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: zIVANtheGREAT
 // 
 // Create Date: 06/05/2024 11:48:50 AM
 // Design Name: 
 // Module Name: InputRead
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
 // 
@@ -24,11 +16,16 @@ module InputRead(
     input  [7:0] data_in,
     output       res1_out,
     output [3:0] res2_out,
-    output [0:7] res3_out
+    output [0:7] res3_out,
+    output [7:0] res4_out,
+    output [7:0] res5_out
     );
-
+    
+//res4 and res5 while result in a barrel shift.
 assign res1_out = data_in[3],
        res2_out = data_in[5:2],
-       res3_out = data_in;
+       res3_out = data_in,
+       res4_out = {data_in, 2'b00}, //feeds 00 from the right.
+       res5_out = {4'b0000, data_in[7:4]}; //feeds 00 from the left.
        
 endmodule
