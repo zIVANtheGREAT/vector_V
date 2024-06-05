d0f28a408ed812382aeaf5c9f7e36e96851c52ff
Ivan <132115436+zIVANtheGREAT@users.noreply.github.com>
2024-06-05 12:50:31 -0700

    Update vector_VTB.v
    Added res4 and res5 to reflect the other modules. this test bench will incorporate the concatenation.

--- sim_1/new/vector_VTB.v
+++ sim_1/new/vector_VTB.v
@@ -1,33 +1,25 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: zIVANtheGREAT
 // 
 // Create Date: 06/05/2024 12:00:06 PM
 // Design Name: 
 // Module Name: vector_VTB
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-
-
 module vector_VTB();
 
 reg  [7:0] dataT;
 wire       res1T;
 wire [3:0] res2T;
 wire [0:7] res3T;
+wire [7:0] res4T, res5T;
 
-vector_Vtop UUT (.data(dataT), .res1(res1T), .res2(res2T), .res3(res3T));
+vector_Vtop UUT (.data(dataT), .res1(res1T),
+                 .res2(res2T), .res3(res3T),
+                 .res4(res4T), .res5(res5T));
 
 initial begin
     dataT = 8'hf3;
