707108b433dfd704acf46366bf49d86dcdd73693
Ivan <132115436+zIVANtheGREAT@users.noreply.github.com>
2024-06-05 12:42:39 -0700

    Update vector_V.v
    Added res4 and res5 for concatenation through the input read module.

--- sources_1/new/vector_V.v
+++ sources_1/new/vector_V.v
@@ -1,17 +1,11 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: zIVANtheGREAT
 // 
 // Create Date: 06/05/2024 11:48:50 AM
 // Design Name: 
 // Module Name: vector_Vtop
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
 // 
 // Revision:
 // Revision 0.01 - File Created
@@ -24,9 +18,13 @@ module vector_Vtop(
     input  [7:0] data,
     output       res1,
     output [3:0] res2,
-    output [0:7] res3
+    output [0:7] res3,
+    output [7:0] res4,
+    output [7:0] res5
     );
     
-InputRead Instance(.data_in(data), .res1_out(res1), .res2_out(res2), .res3_out(res3));
+InputRead Instance(.data_in(data),  .res1_out(res1),
+                   .res2_out(res2), .res3_out(res3),
+                   .res4_out(res4), .res5_out(res5));
 
 endmodule
